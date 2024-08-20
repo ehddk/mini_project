4e40d23a6674571f3a26be577805c5687748a039
Yu-Kyeong-Seok <iq4578@naver.com>
2024-08-20 13:17:17 +0900

    공통단 수정

--- src/weather.ts
+++ src/weather.ts
@@ -1,3 +1,2 @@
 import "./main";
-
 import "./styles/weather.scss";
--- vite.config.js
+++ vite.config.js
@@ -6,21 +6,20 @@ export default {
     rollupOptions: {
       input: {
         // 추가하고 싶은 페이지는 pages에서 추가하면 됨
-        main: path.resolve(__dirname, "index.html"), // 홈
-        task: path.resolve(__dirname, "pages/task.html"), // 할 일
-        addTask: path.resolve(__dirname, "pages/add-task.html"), // 할 일 추가
+        main: path.resolve(__dirname, "index.html"), // 홈=
         movies: path.resolve(__dirname, "pages/movies.html"), // 영화
-        timer: path.resolve(__dirname, "pages/timer.html"), // 타이머
+        todo: path.resolve(__dirname, "pages/todo.html"), // 타이머
         weather: path.resolve(__dirname,"pages/weather.html")//날씨
       },
     },
-    server: {
-        port: 3000,
-    },
-    plugins: [
-        injectHTML(),
-        // handlebars({
-        //   partialDirectory: path.resolve(__dirname, "partials"),
-        // }),
-    ],
+  },
+  server: {
+    port: 3000,
+  },
+  plugins: [
+    injectHTML(),
+    // handlebars({
+    //   partialDirectory: path.resolve(__dirname, "partials"),
+    // }),
+  ],
 };
