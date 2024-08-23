de3f1b7b51348c9923d4d112444c48f6e62b137d
ehddk <bj9147@naver.com>
2024-08-23 11:39:36 +0900

    todo 로컬스토리지 적용 및 home에 출력 완료

--- src/home.ts
+++ src/home.ts
@@ -1,5 +1,24 @@
 import "./main";
 import "./styles/home.scss";
 
-const todoText = document.querySelector('.todo-task');
-todoText.textContent = localStorage.getItem('todo');
+
+const todoEle=document.getElementById('todo_task');
+const todo_task=localStorage.getItem('todo');
+const todos = todo_task ? JSON.parse(todo_task) : [];
+const today=new Date();
+let year=today.getFullYear();
+let month = (today.getMonth() + 1).toString()  // 월을 01이 아니라 1처럼 2자리로 표시
+let date = today.getDate().toString() // 일도 마찬가지.
+let today_date=year + '-' + month+ '-'+date; // 2024-8-22일처럼 나오게 조합
+const filteredTodos=todos.filter((item: { year: any; month: any; day: any; })=>{
+    let Date=`${item.year}-${item.month}-${item.day}`;
+    return Date===today_date;  //오늘 날짜랑 내가 조합한 날짜랑 같은 것만 나오게 return
+});
+
+console.log('fik',filteredTodos)
+const titles= filteredTodos.map((item: { events: any[]; })=>item.events.map(event=>event.title))
+
+todoEle && (todoEle.textContent = titles.length > 0 
+    ? titles.join(', ')  // title이 있을 때 
+    : '등록된 할 일이 없습니다.' // title이 없을 때 
+  );
